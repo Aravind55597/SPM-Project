3c410415bc6fc4ee519d60f44c4f9502c62cf54f
G.Aravind <aravindg.2019@smu.edu.sg>
2021-09-23 10:12:05 +0800

    -Created empty view controllers for ui team to route to their respective pages -Backend set up is done ; just need to add necessary functions for frontend to call to

--- Services/CourseManagementService.cs
+++ Services/CourseManagementService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SPM_Project.Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,24 @@ namespace SPM_Project.Services
 {
     public class CourseManagementService
     {
+
+        public IUnitOfWork _UnitOfWork;
+
+        public CourseManagementService(IUnitOfWork UnitOfWork)
+        {
+            _UnitOfWork = UnitOfWork; 
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
