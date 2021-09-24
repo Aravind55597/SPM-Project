c3d480c6a290247b6febd90ac1195b2c30ae8585
G.Aravind <aravindg.2019@smu.edu.sg>
2021-09-24 19:28:47 +0800

    -Added custom exceptions for service layer : BadRequestException & NotFoundException

---
+++ CustomExceptions/BadRequestException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace SPM_Project.CustomExceptions
+{
+    [Serializable]
+
+    public class BadRequestException:Exception
+    {
+
+        public BadRequestException(string message)
+            : base(message)
+        {
+        }
+        public BadRequestException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+        public BadRequestException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+
+    }
+}
