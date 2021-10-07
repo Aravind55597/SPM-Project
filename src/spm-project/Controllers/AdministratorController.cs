e2f180aafdb834ad8f1895580421df17053ab924
CHINNING <72450579+deathreaper98@users.noreply.github.com>
2021-10-08 01:05:26 +0800

    add: controllers for view-all-classes, view-all-courses, view-all-requests

--- src/spm-project/Controllers/AdministratorController.cs
+++ src/spm-project/Controllers/AdministratorController.cs
@@ -11,17 +11,29 @@ namespace SPM_Project.Controllers
     public class AdministratorController : Controller
     {
         [Authorize(Roles = "Administrator")]
-        public IActionResult ViewEngineers()
+        public IActionResult ViewAllEngineers()
         {
-            return View("ViewEngineers");
+            return View("ViewAllEngineers");
         }
-
 
 
         [Authorize(Roles = "Administrator")]
         public IActionResult ViewAllCourses()
         {
             return View("ViewAllCourses");
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public IActionResult ViewAllClasses()
+        {
+            return View("ViewAllClasses");
+        }
+
+
+        [Authorize(Roles = "Administrator")]
+        public IActionResult ViewAllRequests()
+        {
+            return View("ViewAllRequests");
         }
 
 
