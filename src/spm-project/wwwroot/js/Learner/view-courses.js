5e0e4a2ec13e1fa0ab02a5e73da9c0a4c65b96f6
Xianweiii <68322946+Git-mind@users.noreply.github.com>
2021-11-04 23:14:34 +0800

    fix: fix horizontal bar issue

--- src/spm-project/wwwroot/js/Learner/view-courses.js
+++ src/spm-project/wwwroot/js/Learner/view-courses.js
@@ -15,32 +15,33 @@
 
                 var slots = item.slots
 
-                dataHtml += '<div class="row align-items-center"><div class="col">' + `<div class="card mb-3" style="max-width: 540px; background-color: fefaf4;">
-                        <div class="row g-0">
-                            <div class="col">
-                                <div class="card-body">
-                                    <h5 class="card-title">${className}</h5>
-                                    <p class="card-text">Trainer: ${trainerName}</p>
-                                    <p class="card-text">Class Duration: ${startClass} - ${endClass}</p>
-                                    <p class="card-text">Registration Period: ${startRegistration} - ${endRegistration}</p>
-
-                                    <p class="card-text">Slots Available: ${slots}</p>
-
-
-                                    <p class="card-text"><small class="text-muted"></small></p>
-                                    <div class="row">
-                                        <div class="col"></div>
-                                        <div class="col-6">
-                                            <button type="button" onclick="submitEnrollmentRequest(${classId}, this)"; class="btn-sign-up btn btn-primary w-100">
-                                              Sign up for this class
-                                            </button>
+                dataHtml += `<div class="row align-items-center">
+                                <div class="col">
+                                    <div class="card mb-3" style="max-width: 540px; background-color: fefaf4;">
+                                        <div class="row g-0">
+                                            <div class="col">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">${className}</h5>
+                                                    <p class="card-text">Trainer: ${trainerName}</p>
+                                                    <p class="card-text">Class Duration: ${startClass} - ${endClass}</p>
+                                                    <p class="card-text">Registration Period: ${startRegistration} - ${endRegistration}</p>
+                                                    <p class="card-text">Slots Available: ${slots}</p>
+                                                    <p class="card-text"><small class="text-muted"></small></p>
+                                                    <div class="row">
+                                                        <div class="col"></div>
+                                                        <div class="col-6">
+                                                            <button type="button" onclick="submitEnrollmentRequest(${classId}, this)"; class="btn-sign-up btn btn-primary w-100">
+                                                              Sign up for this class
+                                                            </button>
+                                                        </div>
+                                                        <div class="col"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div class="col"></div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>` + '</div></div>';;
+                            </div>`;
             });
             dataHtml += ``;
             $("#view_classes").html(dataHtml);
@@ -117,23 +118,27 @@ $(function () {
                 var dataHtml = '';
 
                 $.each(response, function (index, item) {
-                    dataHtml += '<div class="row align-items-center"><div class="col">' + `<div class="card mb-3" style="max-width: 540px;">
-                        <div class="row g-0">
-                            <div class="col-md-4">
-                                <img src="..." class="img-fluid rounded-start" alt="...">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">${item.Name}</h5>
-                                    <p class="card-text">${item.Description}</p>
-                                    <p class="card-text"><small class="text-muted"></small></p>
-                                    <button type="button" value="course_id" onclick="view_classes(${item.Id})"; class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                      View Classes Available
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>` + '</div></div>';
+                    dataHtml += `<div class="row align-items-center">
+                                    <div class="col">
+                                        <div class="card mb-3" style="max-width: 540px;">
+                                            <div class="row g-0">
+                                                <div class="col-md-4">
+                                                    <img src="..." class="img-fluid rounded-start" alt="...">
+                                                </div>
+                                                <div class="col-md-8">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">${item.Name}</h5>
+                                                        <p class="card-text">${item.Description}</p>
+                                                        <p class="card-text"><small class="text-muted"></small></p>
+                                                        <button type="button" value="course_id" onclick="view_classes(${item.Id})"; class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                                          View Classes Available
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`;
                 });
 
                 dataHtml += '';
