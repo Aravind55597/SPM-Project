5bb0623f9f2136af92b63ccea5530650a8df1280
Esther-Teo <68456433+Esther-Teo@users.noreply.github.com>
2021-11-08 17:55:33 +0800

    Refactor: Quiz Creation

--- src/spm-project/wwwroot/js/Trainer/SubmitQuiz.js
+++ src/spm-project/wwwroot/js/Trainer/SubmitQuiz.js
@@ -16,19 +16,19 @@ $("#create_MCQqn").click(function () {
 
                                         <textarea class="form-control my-4 ml-2 qnText" id="exampleFormControlTextarea1" rows="2" placeholder="Write question here"></textarea>
                                         <div class="question_options">
-                                            <div class="input-group mb-3">
+                                            <div class="input-group mb-3 qnOption">
                                                 <span class="input-group-text" id="inputGroup-sizing-default">1.</span>
                                                 <input type="text" class="form-control qnOption" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="option1">
                                             </div>
-                                            <div class="input-group mb-3">
+                                            <div class="input-group mb-3 qnOption">
                                                 <span class="input-group-text" id="inputGroup-sizing-default">2.</span>
                                                 <input type="text" class="form-control qnOption" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="option2">
                                             </div>
-                                            <div class="input-group mb-3">
+                                            <div class="input-group mb-3 qnOption">
                                                 <span class="input-group-text" id="inputGroup-sizing-default">3.</span>
                                                 <input type="text" class="form-control qnOption" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="option3">
                                             </div>
-                                            <div class="input-group mb-3">
+                                            <div class="input-group mb-3 qnOption">
                                                 <span class="input-group-text" id="inputGroup-sizing-default">4.</span>
                                                 <input type="text" class="form-control qnOption" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="option4">
                                             </div>
@@ -51,11 +51,11 @@ $("#create_TFqn").click(function () {
 	$("#question_cards").append(`<div class="questions card p-4 m-4">
                                         <textarea class="form-control my-4 ml-2 qnText" id="exampleFormControlTextarea1" rows="2" placeholder="Write question here"></textarea>
                                         <div class="question_options">
-                                            <div class="input-group mb-3">
+                                            <div class="input-group mb-3 qnOption">
                                                 <span class="input-group-text" id="inputGroup-sizing-default">True.</span>
                                                 <input type="text" class="form-control qnOption" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="trueOption">
                                             </div>
-                                            <div class="input-group mb-3">
+                                            <div class="input-group mb-3 qnOption">
                                                 <span class="input-group-text" id="inputGroup-sizing-default">False.</span>
                                                 <input type="text" class="form-control qnOption" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="FalseOption">
                                             </div>
@@ -84,8 +84,7 @@ $("#create_TFqn").click(function () {
 			"ChapterId": null,
 			"CourseClassId": null,
 			"TimeLimit": 20,
-			"Questions": [
-			]
+			"Questions": []
 		};
 		var questionObject = {
 			"ImageUrl": "https://www.howtogeek.com/wp-content/uploads/2018/06/shutterstock_1006988770.png?height=200p&trim=2,2,2,2",
@@ -99,21 +98,26 @@ $("#create_TFqn").click(function () {
 			"Option2": null,
 			"Option3": null,
 			"Option4": null,
-			"IsMultiSelect": false,
+			"IsMultiSelect": false
 		};
 
 		//Check if graded
 		jsObjectForm.Name = $("#quizName").val();
 		jsObjectForm.Description = $("#quizDescription").val();
-		jsObjectForm.TimeLimit = $("#quizDuration").val();
-		if ($('#gradedQuiz').is(':checked')) {
+		jsObjectForm.TimeLimit = parseInt($("#quizDuration").val());
+		/// Issue at finding if checked
+		if ($('.gradedGrp').find('#gradedQuiz').is(':checked')) {
 			jsObjectForm.IsGraded = true;
 			jsObjectForm.ChapterId = null;
 		} else {
+			console.log("test");
 			jsObjectForm.IsGraded = false;
-			jsObjectForm.ChapterId = $("#ChapterID").val();
+			jsObjectForm.ChapterId = parseInt($("#ChapterID").val());
+			console.log($("#ChapterID").val());
+			console.log(typeof (jsObjectForm.ChapterId));
+			console.log(jsObjectForm.ChapterId)
 		};
-		jsObjectForm.CourseClassId = $("#CourseClassID").val();
+		jsObjectForm.CourseClassId = parseInt($("#CourseClassID").val());
 		
 		//Loop through questions
 		$(".questions").each(function () {
@@ -122,18 +126,18 @@ $("#create_TFqn").click(function () {
 			//Retrieve answer from set answers field
 			questionObject.Answer = $(this).find("#qnAnswer").val();
 			//Retrieve marks
-			questionObject.Marks = $(this).find("#qnMarks").val();
+			questionObject.Marks = parseInt($(this).find("#qnMarks").val());
 			//Retrieve option from options (count, if 2 = truefalseqn)
 			if ($(this).find('.qnOption').length > 2) {
 				//MCQ
-				questionObject.QuestionType = "MCQQuestion";
+				questionObject.QuestionType = "McqQuestion";
 				questionObject.Option1 = $(this).find("#option1").val();
 				questionObject.Option2 = $(this).find("#option2").val();
 				questionObject.Option3 = $(this).find("#option3").val();
 				questionObject.Option4 = $(this).find("#option4").val();
 				questionObject.TrueOption = null;
 				questionObject.FalseOption = null;
-				if ($(this).find('#multiselectFalse').is(':checked')) {
+				if ($(this).find('#multiselectTrue').is(':checked')) {
 					questionObject.IsMultiSelect = true;
 				} else {
 					questionObject.IsMultiSelect = false;
@@ -152,16 +156,19 @@ $("#create_TFqn").click(function () {
 			
 			jsObjectForm.Questions.push(questionObject);
 		});
-		
-		
+		console.log(jsObjectForm);
 		$.ajax({
 		   url: '/api/Quizzes',
-		   data: JSON.stringify(jsObjectForm),
+			data: JSON.stringify(jsObjectForm),
+			contentType: 'application/json',
+			type: 'POST',
+			success: function () {
+				alert("success");
+			},
 		   error: function() {
 		      alert("error");
-		   },
-		   dataType: 'jsonp',
-		   type: 'POST'
+		   }
+		   
 		});
 	});
 
